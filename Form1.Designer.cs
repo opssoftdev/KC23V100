4d5c5bb708b415a597c0a71ae0e56e2e35c788f5
opssoftdev <ops_soft@opsweb.co.jp>
2021-01-14 17:04:16 +0900

    今日最初の修正後コミット

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace OPSKC23
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -39,11 +40,19 @@ namespace OPSKC23
             this.textBox1.Size = new System.Drawing.Size(175, 31);
             this.textBox1.TabIndex = 0;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(67, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 31);
+            this.textBox2.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +64,7 @@ namespace OPSKC23
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
